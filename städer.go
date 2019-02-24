889370f4fe82a9a28f6e4d117a0322fd55ffc402
oj_lappi <olappi@abo.fi>
2019-02-24 23:08:10 +0200

    Create städer.go

---
+++ städer.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+        "fmt"
+)
+
+var städer = []string{"Åbo", "Tammerfors","Helsingfors","Pargas","Rovaniemi","Imatra","Kuopio","Hyvinge","S:t Michel", "Uleåborg","Vaasa"}
+func main() {
+
+}
